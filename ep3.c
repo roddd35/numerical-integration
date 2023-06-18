ca3b466b4a98348dbb073a97be2027f9c4e82aa7
Rodrigo Michelassi <rodrigomichelassi@MBP-de-Rodrigo.home>
2023-06-18 17:11:23 -0300

    add lagrange interpolation, trapezoidal and simpson integration functions

--- ep3.c
+++ ep3.c
@@ -1,49 +1,88 @@
 #include <stdio.h>
+#include <math.h>
 
-void atribuiFX(double v[]);
-void atribuiPontosX(int v[], int h);
-/*  
-    adicionar o vetor de pontos nessas funcoes 
-    retornar o vetor com os valores de F interpolados???
-*/
-void interpolaLagrange(double v[]);
-void trapezioC(double v[], double a, double b, double h);
-void simpsomC(double v[], double a, double b, double h);
+double interpolaLagrange(double x);
+double calculaWJ(int j);
+double trapezioC(int r);
+double simpsonC(int r);
+
+int pontosX[] = {0, 5, 10, 15, 20, 25, 30};
+double pontosFX[] = {0.0000, 1.5297, 9.5120, 8.7025, 2.8087, 1.0881, 0.3537};
 
 int main(){
-    int h = 5, i;
-    double a, b;
-    double pontosF[7]; 
-    int pontosX[7];
-    
-    atribuiPontosX(pontosX, h);
-    atribuiFX(pontosF);
+    int r = 5;
 
-    a = pontosX[0]; b = pontosX[6];
+    printf("%lf\n", trapezioC(r));
 
     return 0;
 }
 
-void atribuiPontosX(int v[], int h){
+/* i. interpolacao por lagrange */
+double interpolaLagrange(double x){
     int i;
+    double wj;
+    double px = 0.0;
+    double phi = 1;
     for(i = 0; i < 7; i++){
-        if(i == 0)
-            v[i] = 0;
-        else
-            v[i] = v[i - 1] + h;
+        if(x == pontosX[i])
+            return pontosFX[i];
+        phi *= x - pontosX[i];
     }
+    
+    for(i = 0; i < 7; i++){
+        px += (calculaWJ(i) * pontosFX[i]) / (x - pontosX[i]);
+    }
+    px *= phi;
+
+    return px;
 }
 
-void atribuiFX(double v[]){
-    v[0] = 0.0000; 
-    v[1] = 1.5297;
-    v[2] = 9.5120;
-    v[3] = 8.7025;
-    v[4] = 2.8087;
-    v[5] = 1.0881;
-    v[6] = 0.3537;
+double calculaWJ(int j){
+    int i;
+    double div = 1;
+    for(i = 0; i < 7; i++){
+        if(i != j)
+            div *= pontosX[j] - pontosX[i];
+    }
+    return (1 / div);
 }
 
-void interpolaLagrange(double v[]){
+/* ii. calculo da integral pela regra do trapezio composta */
+double trapezioC(int r){
+    int i;
+    double h = 30.0 / r;
+    double res = 0.0;
+    
+    for(i = 1; i <= r; i++)
+        res += interpolaLagrange(i * h);
 
+    res *= 2;
+    res += interpolaLagrange(0.0) + interpolaLagrange(30.0);
+    res *= h / 2;
+
+    return res;
+}
+
+/* iii. calculo da integral pela regra de simpson composta */
+double simpsonC(int r){
+    int i;
+    double h = 30.0 / r;
+    double res = 0.0;
+    double res1 = res, res2 = res;
+
+    if(!(r % 2))
+        r++;
+
+    for(i = 1; i <= r / 2 - 1; i++)
+        res1 += interpolaLagrange(i * 2 * h);
+    res1 *= 2;
+
+    for(i = 1; i <= r / 2; i++)
+        res2 += interpolaLagrange(i * 2 * h - 1);
+    res2 *= 4;
+
+    res += interpolaLagrange(0.0) + interpolaLagrange(30.0) + res1 + res2;
+    res *= h / 3;
+
+    return res;
 }
